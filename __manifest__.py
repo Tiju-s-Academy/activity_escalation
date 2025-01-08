d61885626f77dcdbd8b013297b21e57c74b14f37
Muhammed Anas <muhammadanaswayanad@gmail.com>
2025-01-08 16:44:37 +0530

    Remove unused view file reference from module manifest

--- __manifest__.py
+++ __manifest__.py
@@ -4,12 +4,11 @@
     'summary': 'Escalate activities if they stay incomplete',
     'description': 'This module escalates activities if they stay incomplete and allows configuration of the timespan for escalation.',
     'author': 'Anas',
-    'depends': ['base', 'mail'],  # Ensure 'mail' is listed as a dependency
+    'depends': ['base', 'mail'],
     'data': [
         'security/ir.model.access.csv',
         'views/activity_escalation_views.xml',
         'views/activity_escalation_config_views.xml',
-        'views/new_view_file.xml',
         'data/activity_escalation_data.xml',
     ],
     'installable': True,
